0fe5f14bbf17d3aca2dcffa49e595145d818c24f
Grace L. Simms <GLS@MacBookAir.bham.rr.com>
2025-10-31 11:43:55 -0500

    fix: Finalize project for Vercel deployment

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    // This makes the Vercel environment variable available to the code
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+  },
+})
